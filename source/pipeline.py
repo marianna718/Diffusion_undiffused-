0a6c9960cf0bc52260e4d75c24198fd4b4a8b18f
unknown <mariannamomchyan@gmail.com>
2024-12-05 19:07:40 -0800

    more function added, please dont read my readme now ;)

--- source/pipeline.py
+++ source/pipeline.py
@@ -177,3 +177,28 @@ def generate(prompt: str, uncond_prompt:str, input_image=None,
     
 
 
+
+def rescale(x, old_range, new_range, clamp = False):
+    old_min, old_max = old_range
+    new_min, new_max = new_range
+
+    x -= old_min
+    x*= (new_max-new_min)/ (old_max - old_min)
+    x += new_min
+    if clamp:
+        x = x.clamp(new_min, new_max)
+    return x
+
+
+def get_time_embedding(timestep):
+    # we will firs define the cosine and sine of our sequances
+    # (160,)
+    freqs = torch.pow(10000, -torch.arange(start=0, end=160, dtype=torch.float32)/160)
+    # (1,160)
+    x = torch.tensor([timestep], dtype=torch.float32)[:,None] * freqs[None]
+    # (1,320)
+    return torch.cat([torch.cos(x), torch.sin(x)], dim =-1)
+
+
+
+
